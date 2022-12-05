ab927682c83e7cbd65be3ad6696cb55f7744e74d
Salla Kaukonen <113294792+salla22008@users.noreply.github.com>
2022-12-05 15:08:41 +0200

    Update src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// Teretulemast
 		
 		Scanner input = new Scanner(System.in);
 		
-		Pesukone olio1 = new Pesukone(true, 1, 2);
+		System.out.println("Käynnistä pesukone valitsemalla 1 tai 2");
+		int kaynnista = Integer.parseInt(input.nextLine());
 		
-		// Käynnistää ohjelmaan, milloin tulostuu myös mitä ohjelmia tarjolla, sitten terminaaliin vaan 1 tai 2 ja enter
+		Pesukone olio1 = new Pesukone(kaynnista);
+		
+		
 		olio1.kaynnista();
 		int ohjelma = input.nextInt();
 		
-		// Valitsee ohjelman, mutta taas vaan pitää tietää et syöttää numeron, en keksi pitäskö tähän mainiin laittaa et se
-		// tulostaa "Valitse ohjelma" mut se ei ois oikein, sit mietin oisko jos laittaa tohon käynnistä tai valitseohjlema metodiin sen
+		olio1.ohjelma = ohjelma;
+		
 		olio1.valitseOhjelma(ohjelma);
 		int lampotila = input.nextInt();
 		olio1.valitseLampotila(lampotila);
@@ -30,27 +32,42 @@ public class Main {
 
 class Pesukone {
 	
-	public boolean kaynnista;
+	public int kaynnista;
 	int ohjelma;
 	int lampotila;
 	
-	public Pesukone(boolean kaynnista, int ohjelma, int lampotila)
+	public Pesukone()
+	{
+		kaynnista = 0;
+		ohjelma = 0;
+		ohjelma = 0;
+	}
+	
+	public Pesukone (int kaynnista)
 	{
 		this.kaynnista = kaynnista;
+	}
+	
+	public Pesukone(int ohjelma, int lampotila)
+	{
 		this.ohjelma = ohjelma;
 		this.lampotila = lampotila;
 	}
 	
 	public void kaynnista()
 	{
-		if (kaynnista == true)
+		if (kaynnista < 1 || kaynnista > 2)
+		{
+			System.out.println("Invalid number");
+		}
+		else if (kaynnista == 1) 
 		{
 			System.out.println("Pesukone käynnistetty ♪♫♪♫");
 			System.out.println("Valitse ohjelma kirjoittamalla '1' tai '2' ");
 			System.out.println("Ohjelma 1 = puuvillapesu");
 			System.out.println("Ohjelma 2 = vauvan vaatteet");
 		}
-		else
+		else if (kaynnista == 2)
 		{
 			System.out.println("Pesukone sammutettu ♪♫♪♫");
 		}
@@ -59,22 +76,19 @@ class Pesukone {
 	
 	public void valitseOhjelma(int ohjelma)
 	{
-		
-		String ohjelma1 = "Puuvillapesu";
-		String ohjelma2 = "Vauvan vaatteet";
-		
+	
 		if (ohjelma < 1 || ohjelma > 2)
 		{
 			System.out.println("Invalid number");
 		}
 		else if (ohjelma == 1)
 		{
-			System.out.println("Valittu ohjelma on " + ohjelma1 + ".");
+			System.out.println("Valittu ohjelma on puuvillapesu.");
 			System.out.println("Valitse lämpötila kirjoittamalla 1 tai 2\nLämpötila 1 = 30°\nLämpötila 2 = 40°");
 		}
 		else if (ohjelma == 2)
 		{
-			System.out.println("Valittu ohjelma on " + ohjelma2 + ".");
+			System.out.println("Valittu ohjelma on vauvan vaatteet.");
 			System.out.println("Valitse lämpötila kirjoittamalla 1 tai 2");
 		}
 		
@@ -105,29 +119,9 @@ class Pesukone {
 	
 	void kaynnistaOhjelma() 
 	{
-		String ohjelma1 = "Puuvillapesu";
-		String ohjelma2 = "Vauvan vaatteet";
-		String lampotila1 = "30°";
-		String lampotila2 = "40°";
-		
-		if (ohjelma == 1)
-		{
-			System.out.println("Ohjelma " + ohjelma1 + " käynnistyy.");
-		}
-		else if (ohjelma == 2)
-		{
-			System.out.println("Ohjelma " + ohjelma2 + " käynnistyy.");
-		}
-		
-		if (lampotila == 1)
-		{
-			System.out.println("Pesun lämpötila on " + lampotila1);
-		}
-		else if (lampotila == 2)
-		{
-			System.out.println("Pesun lämpötila on " + lampotila2);
-		}
-		
+			System.out.println("Ohjelma " + ohjelma + " käynnistyy. Lämpötila on " + lampotila);
 	}
+		
+	
 	
 }
